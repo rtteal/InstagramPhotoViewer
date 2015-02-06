519a74b0c77215ebb70c4564d58b8128cbf3a740
rtteal <rtteal@gmail.com>
2015-02-05 19:52:19 -0800

    Finishing MVP

--- app/src/main/java/taylor/com/instagramphotoviewer/adapters/PhotosAdapter.java
+++ app/src/main/java/taylor/com/instagramphotoviewer/adapters/PhotosAdapter.java
@@ -1,7 +1,14 @@
 package taylor.com.instagramphotoviewer.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -13,7 +20,32 @@ import taylor.com.instagramphotoviewer.model.Photo;
  */
 public class PhotosAdapter extends ArrayAdapter<Photo>{
 
+    private static class ViewHolder{
+        ImageView ivPhotos;
+        TextView tvCaption;
+    }
+
     public PhotosAdapter(Context context, List<Photo> objects) {
         super(context, android.R.layout.simple_list_item_1, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Photo photo = getItem(position);
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_photo, parent, false);
+            viewHolder.ivPhotos = (ImageView) convertView.findViewById(R.id.ivPhoto);
+            viewHolder.tvCaption = (TextView) convertView.findViewById(R.id.tvCaption);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        viewHolder.tvCaption.setText(photo.caption);
+        viewHolder.ivPhotos.setImageResource(0);
+        Picasso.with(getContext()).load(photo.url).into(viewHolder.ivPhotos);
+        return convertView;
     }
 }
